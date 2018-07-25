5397188b5a257154dfec2f3f78a8f861770b4eb2
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-07-25 11:53:01 +0200

    Passing Data to Components Via Routes
    Using a container

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { NavLink, Route, Redirect } from 'react-router-dom';
 
-// Courses component
-import HTML from './courses/HTML';
-import CSS from './courses/CSS';
-import JavaScript from './courses/JavaScript';
+// Courses render
+import CourseContainer from './courses/CourseContainer';
+import { HTMLCourses, CSSCourses, JSCourses } from '../data/courses';
+
 
 const Courses = ({match}) => (
   <div className="main-content courses">
@@ -18,9 +18,9 @@ const Courses = ({match}) => (
     </div>
     
     <Route exact path={match.path} render={ () => <Redirect to={`${match.path}/html`}/> } />
-    <Route path={`${match.path}/html`} component={HTML} />
-    <Route path={`${match.path}/css`} component={CSS} />
-    <Route path={`${match.path}/javascript`} component={JavaScript} />
+    <Route path={`${match.path}/html`} render={() => <CourseContainer data={HTMLCourses} /> } />
+    <Route path={`${match.path}/css`} render={() => <CourseContainer data={CSSCourses} /> } />
+    <Route path={`${match.path}/javascript`} render={() => <CourseContainer data={JSCourses} /> } />
 
   </div>
 );
