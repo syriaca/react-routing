d533e0fca302790537faac923a5eccf3c402bb1c
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-07-25 11:26:46 +0200

    Redirecting a Route

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink, Route } from 'react-router-dom';
+import { NavLink, Route, Redirect } from 'react-router-dom';
 
 // Courses component
 import HTML from './courses/HTML';
@@ -16,7 +16,8 @@ const Courses = () => (
         <li><NavLink to='/courses/javascript'>JavaScript</NavLink></li>
       </ul>
     </div>
-        
+    
+    <Route exact path='/courses' render={ () => <Redirect to='/courses/html'/> } />
     <Route path='/courses/html' component={HTML} />
     <Route path='/courses/css' component={CSS} />
     <Route path='/courses/javascript' component={JavaScript} />
